31ef539745b94c926d01ce54672c5fd0e751b25a
Abdelrahman <said.abdelrahman89@gmail.com>
2023-01-16 22:23:15 +0000

    Added rays

--- 01_raytracing_in_one_weekend/main.cpp
+++ 01_raytracing_in_one_weekend/main.cpp
@@ -1,18 +1,45 @@
 #include "color.hpp"
+#include "ray.hpp"
 #include "vec3.hpp"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  const int width = 256;
-  const int height = 256;
+color ray_color(const ray &r) {
+  vec3 unit_direction = unit_vector(r.direction());
 
+  double t = 0.5 * (unit_direction.y() + 1.0);
+
+  return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+int main(int argc, char *argv[]) {
+  // Image
+  const double aspect_ratio = 16.0 / 9.0;
+  const int width = 400;
+  const int height = (int)(width / aspect_ratio);
+
+  // Camera
+  double viewport_height = 2.0;
+  double viewport_width = viewport_height * aspect_ratio;
+  double focal_length = 1.0;
+
+  point3 origin = point3(0, 0, 0);
+  vec3 horizontal = vec3(viewport_width, 0, 0);
+  vec3 vertical = vec3(0, viewport_height, 0);
+  point3 lower_left_corner =
+      origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+  // Render
   std::cout << "P3\n" << width << ' ' << height << "\n255\n";
 
   for (int j = height - 1; j >= 0; --j) {
     std::cerr << "\rScalines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < width; ++i) {
-      color pixel_color{double(i) / (width - 1), double(j) / (height - 1),
-                        0.25};
+      double u = double(i) / (width - 1);
+      double v = double(j) / (height - 1);
+
+      ray r{origin, lower_left_corner + u * horizontal + v * vertical - origin};
+
+      color pixel_color = ray_color(r);
 
       write_color(std::cout, pixel_color);
     }
